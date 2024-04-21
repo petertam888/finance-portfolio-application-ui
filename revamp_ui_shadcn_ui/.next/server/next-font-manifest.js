6e9003c97a4e6c4c664a827df197e351d693f8f1
petertam712 <petertam0712@hotmail.com>
2024-04-21 11:17:33 +0800

    init new UI design with using ui.shadcn

---
+++ revamp_ui_shadcn_ui/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/petertam/Documents/GitHub/finance-portfolio-application-ui/revamp_ui_shadcn_ui/src/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
---
+++ revamp_ui_shadcn_ui/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"sJkjPvaj0I4rbZpLkGc+CKRmjyny3oCbcrg9ozZOnPI=\"\n}"
